9b99f4666259b053e252737f046b35a66fd1314a
cammac <camm@rocktechcomputer.com>
2019-05-01 16:28:44 -0700

    minor changes to middleware

--- config/middleware.js
+++ config/middleware.js
@@ -3,6 +3,7 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+
 module.exports = server => {
   server.use(express.json());
   server.use(helmet());
