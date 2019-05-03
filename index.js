89df545279aecdbb3bf12a5868dfa183d787b5fd
cammac <camm@rocktechcomputer.com>
2019-05-02 20:03:56 -0700

    modify package.json

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server = require('./server');
 
 const port = process.env.PORT || 5000;
 
-server.listen(port, (req, res) => {
+server.listen(port, () => {
   console.log(`\n*** Server is running on port ${port} ***\n`);
 });
